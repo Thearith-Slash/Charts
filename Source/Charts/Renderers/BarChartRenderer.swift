264f4c9509faf9689f41386e635314cff0412d1f
Chhuon Ek <chhuon@slash.co>
2020-04-06 23:47:24 +0700

    Custom Path

--- Source/Charts/Renderers/BarChartRenderer.swift
+++ Source/Charts/Renderers/BarChartRenderer.swift
@@ -431,7 +431,9 @@ open class BarChartRenderer: BarLineScatterCandleBubbleRenderer
                 context.setFillColor(dataSet.color(atIndex: j).cgColor)
             }
             
-            context.fill(barRect)
+            let bezierPath = UIBezierPath(roundedRect: barRect, byRoundingCorners: [.topLeft, .topRight], cornerRadii: CGSize(width: 3, height: 3))
+            context.addPath(bezierPath.cgPath)
+            context.drawPath(using: .fill)
             
             if drawBorder
             {
